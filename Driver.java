59de0d484dd84fb1f2b888bb3b6ce7ea0cc72540
jainvis <32726365+jainvis@users.noreply.github.com>
2018-03-22 23:29:34 +1100

    Add files via upload
    Version 4.0

--- Driver.java
+++ Driver.java
@@ -6,26 +6,33 @@ public abstract class Driver {
 
 	static Profile adult;
 	static Scanner input = new Scanner(System.in);
+	static Scanner line = new Scanner(System.in);
 
 	public static Profile addProfile(){
-		input.nextLine();
+		
 		System.out.println("Enter Name");
 		String name = input.nextLine();
 		System.out.println("Enter Age");
 		int age = input.nextInt();
+		input.nextLine();
 		System.out.println("Add Status? (Y/N)");
 		String status = null;
-		String opt = input.next();
-		if(opt=="N"){
+		String opt = line.nextLine();
+		if("N".equals(opt)){
 			status = null;
 		}
-		else if(opt=="Y"){
+		else if("Y".equals(opt)){
+			System.out.println("Enter Status: ");
 			status = input.nextLine();
 		}
-
+		
 		if(age<16 && age>2){
-			String[] dependent = ((Child) adult).addDependent();
-			adult = new Child(name, age, status, dependent);
+			System.out.println("Enter Name of First Dependent");
+			String dependent1 = input.nextLine();
+			System.out.println("Enter Name of Second Dependent");
+			String dependent2 = input.nextLine();
+			adult = new Child(name, age, status, new String[]{dependent1, dependent2});
+			((Child) adult).setDependent(dependent1, dependent2);
 		}
 		else if(age>16){
 			adult = new Adult(name, age, status);
@@ -34,41 +41,52 @@ public abstract class Driver {
 
 	}
 
-	public static void displayProfile(String search){
-			System.out.println("Name: "+adult.getName()+" ,Age: "+adult.getAge()+" ,Status: "+adult.getStatus());
+	public static void displayProfile(Profile adult, String search){
+		System.out.println("Name: "+adult.getName()+" ,Age: "+adult.getAge()+" ,Status: "+adult.getStatus());
 	}
-	
+	public static void profileDetails(Profile profile){
+		if(profile instanceof Adult){
+			System.out.println("Name: "+adult.getName()+" ,Age: "+adult.getAge()+" ,Status: "+adult.getStatus());
+			System.out.println("Friend List: /n"+profile.getFriendlist());
+		}
+		if(profile instanceof Child){
+			System.out.println("Name: "+adult.getName()+" ,Age: "+adult.getAge()+" ,Status: "+adult.getStatus());
+			System.out.println("Dependents: \n"+((Child) profile).getDependent());
+			System.out.println("Friend List: \n"+profile.getFriendlist());
+		}
+	}
+
 	public static void updateProfile(Profile adult){
 		int opt = 0;
 		do{
 			opt = uprofilemenu();
-		switch (opt){
-		case 1:
-			System.out.println("Enter new Name");
-			input.nextLine(); //Added to consume any spaces left after nextInt()
-			String nname = input.nextLine();
-			adult.setName(nname);
-			break;
-			
-		case 2:
-			System.out.println("Enter new Age:");
-			int nage = input.nextInt();
-			adult.setAge(nage);
-			break;
-			
-		case 3:
-			System.out.println("Enter new Status: ");
-			input.nextLine();
-			String nstatus = input.nextLine();
-			adult.setStatus(nstatus);
-			break;
-			
-		case 4:
-			break;
-		}
+			switch (opt){
+			case 1:
+				System.out.println("Enter new Name");
+				input.nextLine(); //Added to consume any spaces left after nextInt()
+				String nname = input.nextLine();
+				adult.setName(nname);
+				break;
+
+			case 2:
+				System.out.println("Enter new Age:");
+				int nage = input.nextInt();
+				adult.setAge(nage);
+				break;
+
+			case 3:
+				System.out.println("Enter new Status: ");
+				input.nextLine();
+				String nstatus = input.nextLine();
+				adult.setStatus(nstatus);
+				break;
+
+			case 4:
+				break;
+			}
 		}while(opt!=4);
 	}
-	
+
 	public static void deleteProfile(Profile adult){
 		System.out.println("Are you Sure? (Y/N)");
 		String choice = input.nextLine();
@@ -80,13 +98,24 @@ public abstract class Driver {
 			break;			
 		}
 	}
-	
+
 	public static void addFriend(Profile profile, Profile friend){
-		if(profile instanceof Child && profile.getAge() - friend.getAge() < 4){
-		profile.setFriend(friend.getName());
+		if(profile instanceof Child && profile.getAge() - friend.getAge() < 4 && friend instanceof Child){
+			profile.setFriend(friend.getName());
+		}
+		if(profile instanceof Adult && friend instanceof Adult){
+			profile.setFriend(friend.getName());
 		}
 	}
 	
+	//Taking Assumption that once dependents are added they cannot be altered, we might give edit option
+	//public static void addDependent(Profile profile, Profile dependent){
+		//if(profile instanceof Child && dependent instanceof Adult){
+			//((Child)profile).setDependent();
+		//}
+		
+	//}
+
 	public static int uprofilemenu(){
 		System.out.println("1. Update Name");
 		System.out.println("2. Update Age");
--- MiniNet.java
+++ MiniNet.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class MiniNet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		HashMap<Integer, Profile> list = new HashMap<Integer, Profile>();
+		inbuiltData(list); //In built data for the program
 		Random randomGenerator = new Random();
 		int id = 0;
 		int opt = 0;
@@ -14,58 +15,103 @@ public class MiniNet {
 			switch (opt){
 			case 1:
 				do{
-					id = randomGenerator.nextInt(9999); //Unique ID are required to be generated
+					id = randomGenerator.nextInt(9999); //Unique ID is required to be generated
 				}while(list.containsKey(id));
 				list.put(id, Driver.addProfile());
-				System.out.println("Your Profile is added with Unique ID: "+id);
-				System.out.println("Remember this unique ID to access profile in future\n");
+				if(list.get(id) instanceof Child){
+					String[] d = ((Child) list.get(id)).getDependent();
+					String d1 = d[0];
+					String d2 = d[1];
+					Profile dep1 = profileCheck(list, d1); //To check if the dependent is on SocioNet
+					Profile dep2 = profileCheck(list, d2);
+					boolean check3 = dependentCheck(list,d); //Check: The Dependent is not added in another dependent list
+					if (dep1 == null || dep2 == null || check3 == false){
+						list.remove(id);
+						System.out.println("Either one or both of your Dependents do not exist on SocioNet");
+						System.out.println("Your Profile was not added !!");
+					}
+					break;
+				}
+				else{
+					System.out.println("Your Profile is added with Unique ID: "+id);
+					System.out.println("Remember this unique ID to access profile in future\n");
+				}
 				break;
 
 			case 2:
 				System.out.println("Enter name of the person");
 				String search = input.nextLine();
-				for(int i : list.keySet()){
-					if(list.get(i).getName().contains(search)){
-						Driver.displayProfile(search);}
+				Profile person = profileCheck(list, search);
+				if(person != null){
+					Driver.displayProfile(person, search);}
+				else{
+					System.out.println("Your Connection is not on SocioNet");
 				}
 				break;
-				
+
 			case 3:
 				System.out.println("Enter Your Unique ID to Access");
 				int uid = input.nextInt();
 				if(list.get(uid) != null){
 					int subopt = 0;
 					do{
-							subopt = submenu1();
-							switch (subopt){
-							case 1:
-								Driver.updateProfile(list.get(uid));
-								break;
-							case 2:
-								Driver.deleteProfile(list.get(uid));
-								break;
-							case 3:
-								int subopt2 = submenu2();
-								System.out.println("Enter full name of your Friend: ");
-								String friend = input.nextLine();
-									for(int i : list.keySet()){
-										if(list.get(i).getName().equals(friend)){
-											Driver.addFriend(list.get(uid), list.get(i));
-										}
-										else{System.out.println("Your Friend is not on SocioNet");}
-									}
+						subopt = submenu1();
+						switch (subopt){
+						case 1:
+							Driver.updateProfile(list.get(uid));
+							break;
+						case 2:
+							Driver.deleteProfile(list.get(uid));
+							break;
+						case 3:
+							Driver.profileDetails(list.get(uid));
+							break;
+						case 4:
+							input.nextLine();
+							System.out.println("Enter 'full name' of your Friend: ");
+							String friend = input.nextLine();
+							for(int i : list.keySet()){
+								if(list.get(i).getName().equals(friend)){
+									Driver.addFriend(list.get(uid), list.get(i));
 								}
-								break;
-								
+								else{System.out.println("Your Friend is not on SocioNet");}
+							}
+							break;
+						}
+						break;
+
 					}while (subopt!=4);
 					break;
 				}
 			case 4:
-				
+				break;
 			}
 
-		}while(opt!=5);
+		}while(opt!=4);
 
+	}
+
+	public static Profile profileCheck(HashMap<Integer, Profile> list, String name){
+		Profile person = null;
+		for(int i : list.keySet()){
+			if(list.get(i).getName().contains(name) == true){
+				person = list.get(i);
+			}
+		}
+		return person;
+	}
+	
+	public static boolean dependentCheck(HashMap<Integer, Profile> list, String[] name){
+		boolean check = false;
+		for(int i : list.keySet()){
+			if(list.get(i) instanceof Child){
+			if(((Child)list.get(i)).getDependent().equals(name)){
+				check = true;
+			}
+			else{System.out.println("Your Dependents exists in another profile !");}
+		}
+		}
+		return check;
 	}
 
 	public static int menu(){
@@ -84,18 +130,21 @@ public class MiniNet {
 		System.out.println("------PROFILE MENU------");
 		System.out.println("1. Update Profile");
 		System.out.println("2. Delete Profile");
-		System.out.println("3. Add Friend");
+		System.out.println("3. Profile Details");
+		System.out.println("4. Add Friend");
 		return input.nextInt();
+	}
+	
+	public static void inbuiltData(HashMap<Integer, Profile> data){
+		data.put(1001, new Adult("John Nash",30,"Mathematician"));
+		data.put(1002, new Adult("Eleanor Stier",30,"Nurse"));
+		data.put(1003, new Child("John David Stier", 10, new String[]{"John Nash","Eleanor Stier"}));
+		data.put(1004, new Adult("Alan Turing",42, "Computer Scientist"));
+		data.put(1005, new Adult("Joan Clarke",37,"Cryptanalyst"));
+		data.put(1006, new Adult("Paul Jobs",44, "United States Coastguard"));
+		data.put(1007, new Adult("Clara Hagopian",42,"Armenian"));
+		data.put(1008, new Child("Steve Jobs", 13, new String[]{"Paul Jobs","Clara Hagopian"}));			
 	}
 
-	public static int submenu2(){
-		Scanner input = new Scanner(System.in);
-		System.out.println("------PROFILE MENU------");
-		System.out.println("1. Update Profile");
-		System.out.println("2. Delete Profile");
-		System.out.println("3. Add Friend");
-		System.out.println("4. Add Dependent");
-		return input.nextInt();
-	}
 }
 
